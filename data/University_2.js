1fab998071aa3fbc14df119d3e438214e7688c4e
Aliyah-Mendoza <132292436+Aliyah-Mendoza@users.noreply.github.com>
2023-05-02 02:07:22 +0800

    Add files via upload

---
+++ data/University_2.js
@@ -0,0 +1,8 @@
+var json_University_2 = {
+"type": "FeatureCollection",
+"name": "University_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Name": "Cape Town" }, "geometry": { "type": "Point", "coordinates": [ 18.417615492789619, -33.929680121293529 ] } }
+]
+}
